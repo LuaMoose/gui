fa40ac99554acc1f78607d35495412a54d762f99
LuaMoose <60906086+LuaMoose@users.noreply.github.com>
2023-04-21 16:32:31 -0400

    Update Gui Beta.lua

--- Gui Beta.lua
+++ Gui Beta.lua
@@ -807,12 +807,23 @@ library.createSlider = function(option, parent)
         if typeof(value) ~= "number" then value = 0 end
         value = library.round(value, option.float)
         value = math.clamp(value, self.min, self.max)
-        if self.min >= 0 then
-            option.fill:TweenSize(UDim2.new((value - self.min) / (self.max - self.min), 0, 1, 0), "Out", "Quad", 0.05, true)
+        if KRNL_LOADED then
+            if self.min >= 0 then
+                option.fill.Size =UDim2.new((value - self.min) / (self.max - self.min)
+            else
+                option.fill.Position = UDim2.new((0 - self.min) / (self.max - self.min))
+                option.fill.Size = UDim2.new(value / (self.max - self.min))
+            end
         else
-            option.fill:TweenPosition(UDim2.new((0 - self.min) / (self.max - self.min), 0, 0, 0), "Out", "Quad", 0.05, true)
-            option.fill:TweenSize(UDim2.new(value / (self.max - self.min), 0, 1, 0), "Out", "Quad", 0.1, true)
+            if self.min >= 0 then
+                option.fill:TweenSize(UDim2.new((value - self.min) / (self.max - self.min), 0, 1, 0), "Out", "Quad", 0.05, true)
+            else
+                option.fill:TweenPosition(UDim2.new((0 - self.min) / (self.max - self.min), 0, 0, 0), "Out", "Quad", 0.05, true)
+                option.fill:TweenSize(UDim2.new(value / (self.max - self.min), 0, 1, 0), "Out", "Quad", 0.1, true)
+            end
         end
+
+       
         library.flags[self.flag] = value
         self.value = value
         option.title.Text = (option.text == "nil" and "" or option.text .. ": ") .. option.value .. option.suffix
