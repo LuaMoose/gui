5d7fad0e9f8edb4520036cd2ee4e6cdee093fc5b
LuaMoose <60906086+LuaMoose@users.noreply.github.com>
2023-07-02 19:56:05 -0400

    Update Gui-ExternalSupport.lua

--- Gui-ExternalSupport.lua
+++ Gui-ExternalSupport.lua
@@ -2025,9 +2025,9 @@ function library:Tab(title)
                 end
 
 				function option:Get()
-                    if option.multiselect then
-                        return self.values
-                    end
+                    -- if option.multiselect then
+                    --     return self.value
+                    -- end
 					return option.value
 				end
 
