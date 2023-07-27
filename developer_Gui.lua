0f3c269e1fcf3512cd8eafc68e5cc923b36b6e9b
LuaMoose <60906086+LuaMoose@users.noreply.github.com>
2023-07-27 10:31:08 -0400

    Update developer_Gui.lua

--- developer_Gui.lua
+++ developer_Gui.lua
@@ -924,7 +924,7 @@ library.createList = function(option, parent)
 
     option.content = library:Create("ScrollingFrame", {
         ZIndex = 4,
-        Size = UDim2.new(1, 0, 5, 0),
+        Size = UDim2.new(1, 0, 1, 0),
         BackgroundTransparency = 1,
         BorderSizePixel = 0,
         ScrollBarImageColor3 = Color3.new(),
